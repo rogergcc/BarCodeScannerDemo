5be6c281f179cf0b16ec518a96e08a2949a041cf
Roger Colquehuanca <rogcolquehuancac@gmail.com>
2023-06-26 23:45:53 -0500

    base project run depependencies working

--- app/src/main/java/com/rogergcc/barcodescannerdemo/MainActivity.kt
+++ app/src/main/java/com/rogergcc/barcodescannerdemo/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.rogergcc.barcodescannerdemo
 
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,6 +12,8 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
 import com.rogergcc.barcodescannerdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +25,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        }
+
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
@@ -29,9 +41,35 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
+            toggleDayNight()
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+    }
+
+    private fun toggleDayNight() {
+        when (resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+//                Timber.e("Night Mode")
+                loge("Night Mode")
+            }
+            Configuration.UI_MODE_NIGHT_NO -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    val flags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                    } else {
+                        View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                    }
+                    window.decorView.systemUiVisibility = flags
+                }
+//                Timber.e("Day Mode")
+                loge("Day Mode")
+            }
+        }
+    }
+
+    private fun loge(messageLog: String?) {
+        Log.e("MainActivity", messageLog?:"Nadaaa..")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
