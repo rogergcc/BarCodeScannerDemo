ef1839c922b2e47c06fdf96381e9b0c257ec3432
Roger Colquehuanca <rogcolquehuancac@gmail.com>
2023-11-23 10:49:37 -0500

    partial implementarion barcode overlay - preview DESIGN ONLY

--- app/src/main/java/com/rogergcc/barcodescannerdemo/ui/common/CameraActivity.kt
+++ app/src/main/java/com/rogergcc/barcodescannerdemo/ui/common/CameraActivity.kt
@@ -11,6 +11,9 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import com.google.mlkit.vision.barcode.Barcode
 import com.google.mlkit.vision.barcode.BarcodeScanner
@@ -20,8 +23,10 @@ import com.google.mlkit.vision.common.InputImage
 import com.rogergcc.barcodescannerdemo.databinding.ActivityCameraBinding
 import java.util.concurrent.Executors
 
+
 class CameraActivity : AppCompatActivity() {
 
+    private lateinit var camera: Camera
     private lateinit var binding: ActivityCameraBinding
 
     private var previewView: PreviewView? = null
@@ -31,19 +36,108 @@ class CameraActivity : AppCompatActivity() {
     private var previewUseCase: Preview? = null
     private var analysisUseCase: ImageAnalysis? = null
 
+    lateinit var cameraControl: CameraControl
+    private var flashFlag: Boolean = false
+
+
+    private var showTorchToggle = false
+    private var showCloseButton = false
+    private var useFrontCamera = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        setupCamera()
+        setupEdgeToEdgeUI()
+        applyScannerConfig()
+//        setupCamera() 11
     }
 
-    private fun setupCamera() {
+    private fun applyScannerConfig() {
+
+//        binding.overlayView.setCustomText(R.string.place_the_qr_code_in_the_indicated_rectangle)
+//        binding.overlayView.setCustomIcon(R.drawable.quickie_ic_qrcode)
+//        binding.overlayView.setHorizontalFrameRatio(1.2F) //TODO QR OVERLAY
+        binding.overlayView.setHorizontalFrameRatio(1.9F) //TODO BARCODE OVERLAY
+//        binding.overlayView.setTorchState(true)
+
+        val hasFlash = this.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)
+
+
+        showTorchToggle = true
+        showCloseButton = true
+        useFrontCamera = false
+
+
+        binding.overlayView.setCloseVisibilityAndOnClick(showCloseButton) { finish() }
+//        binding.overlayView.setTorchVisibilityAndOnClick(showTorchToggle) {
+//            remoteView.switchLight()
+//        }
+
+        setupCamera()
+
+
+//        if (showTorchToggle && hasFlash) {
+//            binding.overlayView.setTorchVisibilityAndOnClick(true) {
+////                remoteView.switchLight()
+//                cameraControl.enableTorch(showTorchToggle)
+//                if (camera.cameraInfo.hasFlashUnit()) {
+//                    binding.overlayView.setTorchState(true)
+//                } else {
+//                    binding.overlayView.setTorchState(false)
+//                }
+//            }
+//        }
+
+        if (showTorchToggle && hasFlash) {
+            binding.overlayView.setTorchVisibilityAndOnClick(true) {
+                flashFlag = !flashFlag
+                binding.overlayView.setTorchState(flashFlag)
+                cameraControl.enableTorch(flashFlag)
+
+
+            }
+        }
+
+    }
+
+//    private fun setFlashOperation() {
+//        val hasFlash = this.packageManager
+//            .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)
+//        if (!hasFlash) {
+//            binding.overlayView.setTorchState(false)
+//        } else {
+////            binding.flashBtn.visibility = View.VISIBLE
+//            binding.overlayView.setTorchState(true)
+//        }
+//        binding.flashBtn.setOnClickListener {
+//            if (remoteView.lightStatus) {
+//                remoteView.switchLight()
+//                binding.flashBtn.setImageResource(R.drawable.scankit_flashlight_layer_off)
+//            } else {
+//                remoteView.switchLight()
+//                binding.flashBtn.setImageResource(R.drawable.scankit_flashlight_layer_on)
+//            }
+//        }
+//    }
+
+    private fun setupEdgeToEdgeUI() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.overlayView) { v, insets ->
+            insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                .let { v.setPadding(it.left, it.top, it.right, it.bottom) }
+            WindowInsetsCompat.CONSUMED
+        }
+    }
+
+    private fun setupCamera() {//11
         previewView = binding.previewView
-        cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+        cameraSelector = CameraSelector.Builder()
+
+            .requireLensFacing(lensFacing)
+
+            .build()
         ViewModelProvider(
             this, ViewModelProvider.AndroidViewModelFactory.getInstance(application)
         ).get(CameraXViewModel::class.java)
@@ -77,16 +171,28 @@ class CameraActivity : AppCompatActivity() {
 
         previewUseCase = Preview.Builder()
             .setTargetRotation(previewView?.display?.rotation ?: 0)
+//            .setTargetAspectRatio(AspectRatio.RATIO_16_9)
             .build()
         previewUseCase!!.setSurfaceProvider(previewView!!.surfaceProvider)
 
         try {
+//            val imageCapture = ImageCapture.Builder() // de una foto
+//                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+//                .build()
 
-            cameraProvider!!.bindToLifecycle(
+            camera= cameraProvider!!.bindToLifecycle(
                 this,
                 cameraSelector!!,
-                previewUseCase
+                previewUseCase,
             )
+//            camera.cameraControl.enableTorch(true)
+
+            cameraControl = camera.cameraControl
+//            cameraControl.setZoomRatio(0.9f)
+            cameraControl.enableTorch(flashFlag)
+
+
+
         } catch (illegalStateException: IllegalStateException) {
             Log.e(TAG, illegalStateException.message ?: "IllegalStateException")
         } catch (illegalArgumentException: IllegalArgumentException) {
@@ -107,8 +213,12 @@ class CameraActivity : AppCompatActivity() {
             cameraProvider!!.unbind(analysisUseCase)
         }
 
+//        analysisUseCase.camera.cameraControl.setZoomRatio(0.5f)
+
         analysisUseCase = ImageAnalysis.Builder()
             .setTargetRotation(previewView?.display?.rotation?:0)
+//            .setTargetAspectRatio(AspectRatio.RATIO_16_9)
+//            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
             .build()
 
         // Initialize our background executor
@@ -149,6 +259,7 @@ class CameraActivity : AppCompatActivity() {
         barcodeScanner.process(inputImage)
             .addOnSuccessListener { barcodes ->
                 barcodes.forEach { barcode ->
+
                     val bounds = barcode.boundingBox
                     val corners = barcode.cornerPoints
 
